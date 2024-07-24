779401fc61fce8f5733fc7b3ff940d1654fe15bb
malidev <41malidonmez41@gmail.com>
2024-07-24 18:42:34 +0300

    Fix 2

---
+++ generate_songs_json.bat
@@ -0,0 +1,47 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "songs_folder=songs"
+set "json_file=songs.json"
+
+if exist "%json_file%" del "%json_file%"
+
+echo ^[> "%json_file%"
+
+set "song_count=0"
+for %%f in ("%songs_folder%\*.mp3") do (
+    set /a "song_count+=1"
+)
+
+set "current_song=1"
+for %%f in ("%songs_folder%\*.mp3") do (
+    set "song_name=%%~nf"
+    echo Is this song name correct: !song_name! [!current_song!/!song_count!] (y/n^)
+    set /p confirm=
+    if /i "!confirm!"=="y" (
+        echo     ^{ >> "%json_file%"
+        echo         "name": "!song_name!", >> "%json_file%"
+        echo         "path": "/!songs_folder!/!song_name!.mp3" >> "%json_file%"
+        if !current_song! lss !song_count! (
+            echo     ^}, >> "%json_file%"
+        ) else (
+            echo     ^} >> "%json_file%"
+        )
+    ) else (
+        echo Please enter a new name for the song:
+        set /p new_name=
+        echo     ^{ >> "%json_file%"
+        echo         "name": "!new_name!", >> "%json_file%"
+        echo         "path": "/!songs_folder!/!song_name!.mp3" >> "%json_file%"
+        if !current_song! lss !song_count! (
+            echo     ^}, >> "%json_file%"
+        ) else (
+            echo     ^} >> "%json_file%"
+        )
+    )
+    set /a "current_song+=1"
+)
+
+echo ^] >> "%json_file%"
+
+echo json created successfully
